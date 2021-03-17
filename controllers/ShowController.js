7d7dfe91cd3f10a1fee988f4c8dde159811b99eb
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-16 18:55:12 -0700

    MVP

--- controllers/ShowController.js
+++ controllers/ShowController.js
@@ -1,4 +1,67 @@
 const router = require('express').router
 const Show = require('../models/Show')
 
+// Create
+router.post('/', async(req, res) => {
+    try {
+        const newShow = await Show.create({
+             title: req.body.title,
+             premiere: req.body.premiere,
+             network: req.body.network
+        })
+        res.json(newShow)
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+// Read (Index)
+router.get('/', async(req, res) => {
+    try {
+        const allShows = await Show.find({})
+        res.json(allShows)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// Read (Show)
+router.get('/:id', async(req, res) => {
+    try {
+        const foundShow = await Show.findById(req.params.id)
+        console.log(foundShow)
+        if(foundShow) {
+            res.json(foundShow)
+        }
+    } catch (err) {
+        res.json({
+            msg: "A show with that id hasn't been found"
+        })
+    }
+})
+
+// Update 
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedShow = await Show.findByIdAndUpdate(req.params.id,{
+            title: req.body.title,
+            premiere: req.body.premiere,
+            network: req.body.network
+        })
+        res.json(updatedShow)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedShow = await Show.findByIdAndDelete(req.params.id)
+        res.json(deletedShow)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 module.exports = router
